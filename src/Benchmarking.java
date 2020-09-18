dc54cd074f3a8ec94df6a198180452f98ac6552e
woah <56432210+liyuanboo@users.noreply.github.com>
2020-09-18 15:37:57 -0700

    Edited space

--- src/Benchmarking.java
+++ src/Benchmarking.java
@@ -10,34 +10,49 @@ import java.util.*;
 
 public class Benchmarking{
     public static void main(String[] args)throws Exception {
-        File file = new File("C:\\Users\\CutieBoo\\Desktop\\input1.txt");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Input your file name: ");
+        String filename = keyboard.nextLine();
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String str;
         LinkedList<Integer> list = new LinkedList<>();
 
         //time to insert
         long startInsert = System.nanoTime();
+        int i, j, k, key, temp;
         while ((str = buffer.readLine()) != null) {
-            String read[] = str.split(" ");
-            for(int i = 0; i < read.length; i++) {
-                list.add(Integer.parseInt(read[i]));
+            for (i = 1; i < list.size(); i++){
+                key = list.get(i);
+                j = i - 1;
+                while (j >= 0 && key < list.get(j)){
+                    temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                    j--;
+                }
+            }
+            String[] read = str.split(" ");
+            for(k = 0; k < read.length; k++) {
+                list.add(Integer.parseInt(read[k]));
             }
         }
         long endInsert = System.nanoTime();
         long time_insert = endInsert - startInsert;
-        Collections.sort(list);
 
         //time to find min
         long startMin = System.nanoTime();
         int min = list.getFirst();
         long endMin = System.nanoTime();
         long time_min = endMin - startMin;
+        System.out.println();
         System.out.println("min: " + min);
 
         //time to find max
-        Long startMax = System.nanoTime();
-        int max = list.getLast();
-        Long endMax = System.nanoTime();
+        long startMax = System.nanoTime();
+        int max = list.get(list.size() - 2);
+        long endMax = System.nanoTime();
         long time_max = endMax - startMax;
         System.out.println("max: " + max);
 
